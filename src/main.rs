0b11b0770309e95bb648b2ad07433e4811d347e2
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-07-21 22:03:46 +0530

    octocrab testing -1

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,17 @@
-mod modules;
-use serde::Deserialize;
 use tokio;
+use octocrab::Octocrab;
+use std::env;
 
+#[tokio::main]
+async fn main()-> Result<(),Box<dyn std::error::Error>> {
+    //i don't understand it. just that it is needed for heaby tcp calls
+    rustls::crypto::ring::default_provider().install_default().unwrap();
+    //in this we make our clint same as request. it takes your github token and returns the result.
+    let octocrab = Octocrab::builder().personal_token(env::var("GITHUB_TOKEN")?).build()?;
+    let repo = octocrab.repos("abhinavAwasthi9189","IceFinder-ISRO-BAH2026-KalkiCoders").get().await?;
+    //                          ^_username                     ^_RepoName
+    println!("Stars: {:?}", repo.stargazers_count);
+    println!("Description: {:?}", repo.description);
 
-//this is just a struct that stores the info we get back.
-#[derive(Deserialize,Debug)]
-struct git{
-    login:String,public_repos:u32,followers:u32}
-
-fn main() {
-    println!("enter the username");
-    let user = strput();
-    //this is the runtime we prepare. as we don't want to make whole main function async. we create
-    //a seprate small one inside of it
-    let gt = tokio::runtime::Runtime::new().unwrap();
-    
-    //here blockon is like let whats inside of me happen[which is async. and then return what we got
-    //back to synchronous function.
-    let end_value = gt.block_on(async{
-        match getGit(&user).await{
-            Ok(value) => {value}
-            Err(e) => {panic!("Error fetching data: {}", e);}
-        }}
-    );
-
-    println!("User: {}, Repos: {}, Followers: {}", end_value.login, end_value.public_repos, end_value.followers); 
-}
-
-async fn getGit(user:&String) -> Result<git,Box<dyn std::error::Error>>{
-    let url = format!("https://api.github.com/users/{}",user);
-    //we create a new reqwest client
-    let client = reqwest::Client::new();
-    //git somehow needs this header to work. i will read of it later. this  fucntion sends it to api
-    //and when it gets a json result. it returns a git struct object.
-    let value = client.get(&url).header("User-Agent", "rusty-boi").send().await?.json::<git>().await?;
-    Ok(value)
-}
-
-fn strput() -> String {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Failed to read line");
-    input.trim().to_string()
+    Ok(())
 }
