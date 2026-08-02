949f2f7aedfa966b49d516e9130cc6e1d1dafb63
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-08-03 00:19:28 +0530

    bevy testing -2

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use std::sync::atomic::{AtomicU32, Ordering};
 
 #[derive(Component)]
 struct Person;
@@ -6,39 +7,69 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-fn main(){
+#[derive(Resource)]
+struct GT(Timer);
+
+#[derive(Resource, Default)]
+struct TickCounter(u32);
+
+#[derive(Resource, Default)]
+struct time_click(Option<f32>);
+
+fn main() {
     //app is our container/ the game object. it is what the game is inside of.
     //ECS-entity component system[for each object you give its properties seprately from a curated set
 
-    App::new().add_systems(Startup,add_people)
-        .add_systems(Update,(first,hello,change,hello).chain()).run();
-    //adds_system adds systems to our apps schedule(a set of systems,metadata& exectuotors that are
-    //run in a certain sequence().
-    //we can also add methods like .before/.after(system/function) and it will run it before/after that.
-    //(put all functions inside of the second arguement to run them all)
-    
-    //Startup only runs once so use it for things like spawning
-    //Update clocks in 60fps. so it is used for things like keyboard input,etc.
-    
-    //chain() => it makes all the systems/executors in the () its put on to run one by one. else
-    //they can go parallel.
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(GT(Timer::from_seconds(1.0, TimerMode::Repeating)))
+        .init_resource::<time_click>()
+        .add_plugins(NeoPlug)
+        .run();
 }
 
-fn first(){println!("hakuna matata");}
+pub struct NeoPlug;
+
+impl Plugin for NeoPlug {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, /*(change, (hello, change).chain())*/ persec);
+    }
+}
+
+fn first() {
+    println!("hakuna matata");
+}
 
 fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Miri".to_string())));
     commands.spawn((Person, Name("Siya".to_string())));
-    commands.spawn((Person, Name("Shreya".to_string())));
+    commands.spawn((Person, Name("Shraddha".to_string())));
 }
 
 //so it is taking all the query names. that also have person as a component
-fn hello(query:Query<&Name, With<Person>>){
-    for i in query{println!("{}", i.0)};}
+fn hello(query: Query<&Name, With<Person>>) {
+    static TIME: AtomicU32 = AtomicU32::new(0);
+    if TIME.load(Ordering::Relaxed) > 60 {
+        for i in query {
+            println!("{}", i.0)
+        }
+        TIME.fetch_sub(60, Ordering::Relaxed);
+    }
+    TIME.fetch_sub(1, Ordering::Relaxed);
+}
 
-fn change(query: Query<&mut Name, With<Person>>){
-    for mut i in query{
-        if "Miri"==i.0{
+fn persec(time: Res<Time>, mut gt: ResMut<GT>, mut click: ResMut<time_click>) {
+    gt.0.tick(time.delta());
+    if gt.0.just_finished() {
+        click.0 = Some(time.elapsed_secs());
+        println!("{}", (click.0).unwrap() as i32);
+    }
+}
+
+fn change(mut query: Query<&mut Name, With<Person>>) {
+    for mut i in query {
+        if "Miri" == i.0 {
             i.0 = "little Miri".to_string();
         }
     }
