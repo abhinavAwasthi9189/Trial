65ece80a5304f2dae017313e71fc1cdbcebd1cb1
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-08-03 00:27:51 +0530

    bevy testing -2,5

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ struct Name(String);
 #[derive(Resource)]
 struct GT(Timer);
 
+//these are resources, unlike componenets a certain resource can only be one in code. the default
+//makes so that we have a default/ remember the default value.
 #[derive(Resource, Default)]
 struct TickCounter(u32);
 
@@ -22,6 +24,10 @@ fn main() {
 
     App::new()
         .add_plugins(DefaultPlugins)
+        // bith insert/init allow us to insert the resources. unlike insert, init can be initialized
+        // without assigning value. so it just puts in the default value.
+        // here GT gets a timer type value which as a limit of 1.0 sec so above it and ot goes back
+        // to 0.0 repeating means we can keep doing it.
         .insert_resource(GT(Timer::from_seconds(1.0, TimerMode::Repeating)))
         .init_resource::<time_click>()
         .add_plugins(NeoPlug)
@@ -48,6 +54,7 @@ fn add_people(mut commands: Commands) {
 }
 
 //so it is taking all the query names. that also have person as a component
+//this is wrong but i was just trying stuff.
 fn hello(query: Query<&Name, With<Person>>) {
     static TIME: AtomicU32 = AtomicU32::new(0);
     if TIME.load(Ordering::Relaxed) > 60 {
@@ -59,9 +66,16 @@ fn hello(query: Query<&Name, With<Person>>) {
     TIME.fetch_sub(1, Ordering::Relaxed);
 }
 
+//here Res allows us to get resources. unlike componets which all are in Query. they have to be
+//taken seprately. even choose if mutable or immutable.
 fn persec(time: Res<Time>, mut gt: ResMut<GT>, mut click: ResMut<time_click>) {
+    // this function just adds what more value we have gotten from last time to the gt value
     gt.0.tick(time.delta());
+    // and here just finished checks if after that addition did the value went beyond 1 as set
+    // before.
     if gt.0.just_finished() {
+        // elapsed value allows us to get a time components.
+        // we need to do  .0 to access the value if yoy struct name(datatype) it,
         click.0 = Some(time.elapsed_secs());
         println!("{}", (click.0).unwrap() as i32);
     }
